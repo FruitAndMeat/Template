cbec4da7c1051b608597d1e915dcff2ce652324d
FruitAndMeat <92416699+FruitAndMeat@users.noreply.github.com>
2022-07-28 17:29:45 +0800

    添加项目文件。

---
+++ CommunicationLib/Variable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicationLib
+{
+    public class Variable
+    {
+        public string Name { get; set; }
+        public DataType DataType { get; set; }
+        public string Address { get; set; }
+        public string value { get; set; }
+    }
+
+    public enum DataType
+    {
+        Bool,
+        Byte,
+        Word,
+        DWord,
+        Int16,
+        UInt16,
+        Int32,
+        UInt32,
+        Float,
+        Double
+    }
+}
